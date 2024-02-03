20e38160ac6e07036285c32bd06bf3e89b026318
Artem Garanin <artemgar.garanin@gmail.com>
2024-02-03 12:31:14 +0300

    Добавьте файлы проекта.

---
+++ 1 лаба ссп/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace _1_лаба_ссп
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(271, 170);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 27);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.FontChanged += new System.EventHandler(this.textBox1_FontChanged);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DisplayMember = "илюха";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(150, 245);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Tag = "";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(359, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Жирность";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(529, 244);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Наклон";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DisplayMember = "илюха";
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(150, 306);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(151, 28);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.Text = "Размер";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(359, 306);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(151, 28);
+            this.comboBox3.TabIndex = 6;
+            this.comboBox3.Text = "Цвет текста";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(242, 31);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(290, 120);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(543, 308);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(101, 24);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ 1 лаба ссп/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_лаба_ссп
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            String[] font = new string[] { "Segoe UI", "Arial", "Times New Roman", "Segoe Script" };
+            String[] size = new string[] { "9", "12", "15", "18" };
+            String[] colors = new string[] { "Черный", "Красный", "Желтый", "Синий" };
+            InitializeComponent();
+            comboBox1.Items.AddRange(font); 
+            comboBox2.Items.AddRange(size);
+            comboBox3.Items.AddRange(colors);
+            comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 0;
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           // label1.Text = textBox1.Text;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string a = comboBox1.Items[comboBox1.SelectedIndex].ToString();
+            textBox1.Font = new Font(a, textBox1.Font.Size,textBox1.Font.Style);
+            richTextBox1.SelectionFont = new Font(a, richTextBox1.SelectionFont.Size, richTextBox1.SelectionFont.Style);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Font currentfont = richTextBox1.SelectionFont;
+            if (textBox1.Font.Bold == false)
+            {
+                textBox1.Font = new Font(textBox1.Font.FontFamily, textBox1.Font.Size, FontStyle.Bold);
+                richTextBox1.SelectionFont = new Font(currentfont, FontStyle.Bold);
+                button1.BackColor = System.Drawing.Color.DarkGray; 
+            }
+            else
+            {
+                textBox1.Font = new Font(textBox1.Font.FontFamily, textBox1.Font.Size, FontStyle.Regular);
+                richTextBox1.SelectionFont = new Font(currentfont , FontStyle.Regular);
+                button1.BackColor = System.Drawing.Color.White ;
+            }
+        } 
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Font currentfont = richTextBox1.SelectionFont;
+            if (textBox1.Font.Italic == false)
+            {
+                textBox1.Font = new Font(textBox1.Font.FontFamily, textBox1.Font.Size, FontStyle.Italic);
+                richTextBox1.SelectionFont = new Font(currentfont, FontStyle.Italic);
+            }
+            else { textBox1.Font = new Font(textBox1.Font.FontFamily, textBox1.Font.Size, FontStyle.Regular);
+                richTextBox1.SelectionFont = new Font(currentfont, FontStyle.Regular);
+            }
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int.TryParse ( comboBox2.Items[comboBox2.SelectedIndex].ToString(), out int b);
+            textBox1.Font = new Font(textBox1.Font.FontFamily, b, textBox1.Font.Style);
+            richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont.FontFamily, b, richTextBox1.SelectionFont.Style);
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string c = comboBox3.Items[comboBox3.SelectedIndex].ToString();
+            switch (c)
+            {
+                case "Черный":
+                    textBox1.ForeColor = System.Drawing.Color.Black;
+                    //richTextBox1.ForeColor = System.Drawing.Color.Black;
+                    richTextBox1.SelectionColor = System.Drawing.Color.Black;
+                    break;
+                case "Красный":
+                    textBox1.ForeColor = System.Drawing.Color.Red;
+                    //richTextBox1.ForeColor = System.Drawing.Color.Red;
+                    richTextBox1.SelectionColor = System.Drawing.Color.Red;
+                    break;
+                case "Желтый":
+                    textBox1.ForeColor = System.Drawing.Color.Gold;
+                    // richTextBox1.ForeColor = System.Drawing.Color.Gold;
+                    richTextBox1.SelectionColor = System.Drawing.Color.Gold;
+                    break;
+                case "Синий":
+                    textBox1.ForeColor = System.Drawing.Color.Blue;
+                    // richTextBox1.ForeColor = System.Drawing.Color.Blue;
+                    richTextBox1.SelectionColor = System.Drawing.Color.Blue;
+                    break;
+                default :
+                    textBox1.ForeColor = System.Drawing.Color.Black;
+                    // richTextBox1.ForeColor = System.Drawing.Color.Black;
+                    richTextBox1.SelectionColor = System.Drawing.Color.Black;
+                    break;
+            }
+            
+        }
+
+        private void textBox1_FontChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+       
+        }
+
+        bool isBoldEnabled = false; // Переменная для отслеживания состояния переключателя "Жирный/Не жирный"
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            isBoldEnabled = checkBox1.Checked;
+
+            if (isBoldEnabled)
+            {
+                // richTextBox1.SelectionStart = richTextBox1.Text.Length ; // Устанавливаем курсор в конец текста
+                //   richTextBox1.SelectionLength = 0; // Снимаем выделение
+                // Применяем жирное форматирование к новому тексту
+                richTextBox1.Font = new Font(richTextBox1.Font.FontFamily , richTextBox1.Font.Size, FontStyle.Bold);
+            }
+            else
+            {
+                // Если переключатель не активен, используем обычное форматирование
+                richTextBox1.Font = new Font(richTextBox1.Font, FontStyle.Regular);
+            }
+        }
+
+       
+    }
+}
