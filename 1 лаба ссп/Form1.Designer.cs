26710124de1ff83d312f2265d056c3bdb77f5957
Artem Garanin <artemgar.garanin@gmail.com>
2024-03-29 15:35:28 +0300

    недоделанное доп  задание

--- 1 лаба ссп/Form1.Designer.cs
+++ 1 лаба ссп/Form1.Designer.cs
@@ -41,6 +41,10 @@ namespace _1_лаба_ссп
             this.button3 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button4 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -147,12 +151,55 @@ namespace _1_лаба_ссп
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(601, 62);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(151, 28);
+            this.comboBox2.TabIndex = 16;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(621, 241);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 29);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "Чтение";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(621, 301);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(94, 29);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "Запись";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(612, 176);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(152, 29);
+            this.button7.TabIndex = 19;
+            this.button7.Text = "Сохранение стиля";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
@@ -182,6 +229,10 @@ namespace _1_лаба_ссп
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- 1 лаба ссп/Form1.cs
+++ 1 лаба ссп/Form1.cs
@@ -7,24 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Text.Json;
 
 namespace _1_лаба_ссп
 {
     public partial class Form1 : Form
     {
-        String[] font =  { "Segoe UI", "Arial", "Times New Roman", "Segoe Script", "Broadway", "Tahoma" };
+        String[] font =  { "Segoe UI", "Arial", "Times New Roman", "Segoe Script", "Tahoma" };
 
         Font lastFont = new Font("Segoe UI",9);
         Color lastColor = Color.Black;
+
+
+        List<Font> listfont = new List<Font> { };
+        int combobox2;
+
+        static string path1 = @"C:\note.txt";   // путь к файлу
+        static string path2 = @"C:\set.txt";
+      
         public Form1()
         {
+
             //String[] font = new string[] { "Segoe UI", "Arial", "Times New Roman", "Segoe Script", "Broadway", "Tahoma" };
             InitializeComponent();
             comboBox1.Items.AddRange(font);
             comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             comboBox1.SelectedIndex = 0;
             richTextBox1.SelectionChanged += richTextBox1_SelectionChanged;
-        }
+            //SaveFileDialog saveFileDialog = new SaveFileDialog();
+            // saveFileDialog.Filter = 
+        } 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             //if (comboBox1.Items == null) comboBox1.SelectedIndex = 0;
@@ -47,7 +60,12 @@ namespace _1_лаба_ссп
                 richTextBox1.SelectionLength = startlen;
             }
             richTextBox1.Focus();
-            if (richTextBox1.Text.Length != 0) lastFont = richTextBox1.SelectionFont;
+            if (richTextBox1.Text.Length != 0)
+            {
+                lastFont = richTextBox1.SelectionFont;
+              //  listfont.Add(richTextBox1.SelectionFont);
+               // combobox2++;
+            }
         }
 
         private void button1_Click_1(object sender, EventArgs e)
@@ -71,7 +89,12 @@ namespace _1_лаба_ссп
                 richTextBox1.SelectionLength = startlen;
             }
             richTextBox1.Focus();
-            if (richTextBox1.Text.Length != 0) lastFont = richTextBox1.SelectionFont;
+            if (richTextBox1.Text.Length != 0)
+            {
+                lastFont = richTextBox1.SelectionFont;
+              //  listfont.Add(richTextBox1.SelectionFont);
+              //  combobox2++;
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -95,7 +118,12 @@ namespace _1_лаба_ссп
                 richTextBox1.SelectionLength = startlen;
             }
             richTextBox1.Focus();
-            if (richTextBox1.Text.Length != 0) lastFont = richTextBox1.SelectionFont;
+            if (richTextBox1.Text.Length != 0)
+            {
+                lastFont = richTextBox1.SelectionFont;
+              //  listfont.Add(richTextBox1.SelectionFont);
+             //   combobox2++;
+            }
         }
 
         private FontStyle Use_BoldStyle()
@@ -157,7 +185,7 @@ namespace _1_лаба_ссп
                 }
             }
 
-        }
+        }   
 
         private void button3_Click(object sender, EventArgs e)
         {
@@ -182,7 +210,12 @@ namespace _1_лаба_ссп
             }
             richTextBox1.Focus();
             // lastFont = richTextBox1.SelectionFont;
-            if (richTextBox1.Text.Length != 0) lastColor = richTextBox1.SelectionColor;
+            if (richTextBox1.Text.Length != 0)
+            {
+                lastColor = richTextBox1.SelectionColor;
+             //   listfont.Add(richTextBox1.SelectionFont);
+              //  combobox2++;
+            }
         }
 
 
@@ -217,7 +250,12 @@ namespace _1_лаба_ссп
                 richTextBox1.SelectionLength = startlen;
             }
             richTextBox1.Focus();
-            if (richTextBox1.Text.Length != 0) lastFont = richTextBox1.SelectionFont;
+            if (richTextBox1.Text.Length != 0)
+            {
+                lastFont = richTextBox1.SelectionFont;
+              //  listfont.Add(richTextBox1.SelectionFont);
+              //  combobox2++;
+            }
         }
         private void richTextBox1_SelectionChanged(object sender, EventArgs e)
         {
@@ -243,28 +281,28 @@ namespace _1_лаба_ссп
                 else button1.BackColor = System.Drawing.Color.White;
                 if (richTextBox1.SelectionFont.Italic == true) button2.BackColor = System.Drawing.Color.DarkGray;
                 else button2.BackColor = System.Drawing.Color.White;
-                if (richTextBox1.Text.Length != 0) { 
-                    //lastFont = richTextBox1.SelectionFont;
-                   // lastColor = button3.BackColor;
-                }
             }
+            
         }
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
+
             if (richTextBox1.Text.Length == 0)
             {
                 richTextBox1.SelectionFont = lastFont;
                 richTextBox1.SelectionColor = lastColor;
                 ChangedParam(lastFont, lastColor);
             }
+
         }
 
         private void ChangedParam(Font changeFont, Color color)
         {
             if (changeFont != null)
             {
-                textBox1.Text = changeFont.Size.ToString(); // Update the TextBox with the font size
+                textBox1.Text = changeFont.Size.ToString();
+                richTextBox1.SelectionFont = new Font(richTextBox1.SelectionFont.FontFamily,changeFont.Size);
                 if (color == Color.Black)
                 {
                     button3.BackColor = Color.Black;
@@ -288,6 +326,57 @@ namespace _1_лаба_ссп
                     //lastFont = richTextBox1.SelectionFont;
                     // lastColor = button3.BackColor;
                 }
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int a = comboBox2.SelectedIndex;
+            ChangedParam(listfont[a], Color.Black);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            using (FileStream fstream = File.OpenRead(path1))
+            {
+                // выделяем массив для считывания данных из файла
+                byte[] buffer = new byte[fstream.Length];
+                // считываем данные
+                fstream.Read(buffer);
+                // декодируем байты в строку
+                string textFromFile = UTF32Encoding.Default.GetString(buffer);
+                richTextBox1.Text = textFromFile;
+            }
+           
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            using (FileStream fstream = new FileStream(path1, FileMode.Truncate))
+            {
+                // преобразуем строку в байты
+                byte[] buffer = Encoding.Default.GetBytes(richTextBox1.Text);
+                // запись массива байтов в файл
+                fstream.Write(buffer, 0, buffer.Length);
+            }
+            using (FileStream fstream = new FileStream(path2, FileMode.Truncate))
+            {
+                // преобразуем строку в байты
+                string jsonString = JsonSerializer.Serialize(listfont);
+                byte[] buffer = Encoding.Default.GetBytes(jsonString);
+                // запись массива байтов в файл
+                fstream.Write(buffer);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text.Length != 0)
+            {
+                listfont.Add(richTextBox1.SelectionFont);
+                combobox2++;
+                comboBox2.Items.Add("Стиль" + combobox2);
             }
         }
     }
